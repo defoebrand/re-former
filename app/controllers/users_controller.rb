ed47e715e9e0e8d1e6bc6b0adb0e296ae8a96891
Defoe.Brand <defoe.brand@gmail.com>
2020-08-31 16:05:44 +0300

    Set up HTML form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-  def new
-  end
+  include UsersHelper
+  def new; end
 
   def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 end
